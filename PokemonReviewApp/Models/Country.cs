201b27acc30805c7bf6d1cbc12fd858cb8590d35
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-04-18 11:30:59 +0300

    defined the models

---
+++ PokemonReviewApp/Models/Country.cs
@@ -0,0 +1,10 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Owner> Owners { get; set;}
+
+    }
+}
