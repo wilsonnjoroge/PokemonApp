375e856ca0b00cced017e4269a6f74df6c1a4571
Wilson Njoroge Wanderi <wilsonnjoroge932@gmail.com>
2024-05-03 23:30:07 +0300

    Expounded on the app for practice

--- PokemonReviewApp/Models/Owner.cs
+++ PokemonReviewApp/Models/Owner.cs
@@ -6,5 +6,8 @@
         public string Name { get; set; }
         public string Gym { get; set; }
         public Country Country { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
     }
 }
--- PokemonReviewApp/Models/pokemon.cs
+++ PokemonReviewApp/Models/pokemon.cs
@@ -6,6 +6,8 @@
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
 
 
     }
